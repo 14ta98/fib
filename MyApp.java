c5141a1a7df0516c983813737a55c57ec54e5041
vagrant <vagrant@localhost.localdomain>
2017-07-30 08:42:05 +0900

    [add] フィボナッチ数列プログラムを作成しました。
    [note] クラスを別ファイルに定義していない状態です。

---
+++ MyApp.java
@@ -0,0 +1,25 @@
+public class MyApp {
+
+  public static void main(String[] args) {
+    fib(12);
+  }
+  // フィボナッチ数列の処理
+  public static void fib(int inputNum) {
+    int a = 0;
+    int b = 1;
+    int c = 0;
+    int[] resultNums = new int[inputNum];
+    for (int i = 0; i < inputNum;  i++) {
+      resultNums[i] = a;
+      c = a + b;
+    	a = b;
+    	b = c;
+    }
+    String totalNum = "";
+    for (int j : resultNums) {
+      totalNum += j + " ";
+    }
+      System.out.println(inputNum + " 番目のフィボナッチ数: " + c);
+      System.out.println(inputNum + " 番目までのフィボナッチ数列: " + totalNum);
+    }
+}
